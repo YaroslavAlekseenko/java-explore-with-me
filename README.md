64cc8c07cf184af4447095a2f1268683f98eeb67
YaroslavAlekseenko <yar.a.alekseenko@yandex.ru>
2023-06-23 00:35:57 +0300

    "Explore-with-me v.3.0"
    "feat: добавить comments — комментарии к событиям"

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-explore-with-me
 Template repository for ExploreWithMe project.
+
+https://github.com/YaroslavAlekseenko/java-explore-with-me/pull/7
---
+++ main_svc/src/main/java/ru/practicum/main/dto/comment/NewCommentDto.java
@@ -0,0 +1,17 @@
+package ru.practicum.main.dto.comment;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NewCommentDto {
+    @NotBlank
+    @Size(min = 20)
+    private String text;
+}
